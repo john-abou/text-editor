00c18a0e009f3d847739338d38b1212d41ef4784
john-abou <abounassar.john@gmail.com>
2023-03-16 20:45:40 -0400

    Fix: Fixed the database bug causing a typeError.
    updated the GET function to have cases for empty data.
    Updated the PUT function to account for the ID.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,20 +17,22 @@ export const putDb = async (content) => {
   const textDb = await openDB('jate', 1);
   const tx = textDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({  text: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('Updated one: ', result);
 };
 
 export const getDb = async () => {
-  console.log('GET all from jate');
-  const db = await openDB('jate', 1);
-  const tx = db.transaction('jate', 'readonly');
+  console.log('GET from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
-  console.log('data retrieved: ', result);
-  return result;
+  result
+    ? console.log('data from database: ', result.value)
+    : console.log('no database found');
+  return result?.value;
 };
 
 initdb();
